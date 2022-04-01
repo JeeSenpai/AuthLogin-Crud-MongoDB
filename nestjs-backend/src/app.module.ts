1b75a5a6e43d051655f031d451fedb2e470fcae4
JeeSenpai <100999803+JeeSenpai@users.noreply.github.com>
2022-04-01 17:10:49 +0800

    Third Commit
    Adding Authentication

--- nestjs-backend/src/app.module.ts
+++ nestjs-backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PublicationsModule } from './publications/publications.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PublicationsModule, MongooseModule.forRoot('mongodb://127.0.0.1:27017/mydb')],
+  imports: [PublicationsModule, MongooseModule.forRoot('mongodb://127.0.0.1:27017/mydb'), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
